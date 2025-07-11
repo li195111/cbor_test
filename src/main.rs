914fc9b2deae4ce36f01421a8ece434a1013652d
Yue Li <green07111@gmail.com>
2025-07-11 21:19:02 +0800

    [refactor] 重構資料框架，優化接收邏輯

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use std::{ thread::sleep, time::Duration, vec };
+#[allow(unused_imports)]
 use cobs::encode;
 use serde::{ Serialize, Deserialize };
 use crc::{ Crc, CRC_16_USB };
 
 const BAUD: u32 = 460_800;
 const CRC16: Crc<u16> = Crc::<u16>::new(&CRC_16_USB);
-const START_BYTE: u8 = 0x7E; // 開始 byte
+const START_BYTE: u8 = 0x7e; // 開始 byte
+const MAX_DATA_LEN: usize = 256;
+
+enum CMD {
+    SEND = 0xaa, // 發送資料
+    READ = 0xa8, // 讀取資料
+}
+
+#[repr(u8)]
+enum Command {
+    ACK = 0x01, // 確認收到
+    NACK = 0x02, // 未確認收到
+    MOTOR = 0x03, // 馬達控制
+    SetID = 0x04, // 設定 ID
+    FILE = 0x05, // 檔案傳輸
+    SensorHIGH = 0x06, // 高位元感測器
+    SensorLOW = 0x07, // 低位元感測器
+}
 
 /// 對應 Arduino encode_cbor() 內兩個 9 元素子陣列
 #[derive(Debug, Serialize, Deserialize)]
@@ -28,14 +46,14 @@ struct Motion {
 
 fn build_frame(payload: &[u8]) -> Vec<u8> {
     let mut frame = Vec::with_capacity(payload.len() + 5);
-    
+
     frame.push(START_BYTE); // 開始 byte
-    // Version Byte
-    frame.push(0x01); // 版本號，假設為 1
+    // Cmd Byte, SEND: 0xAA, READ: 0xA8
+    frame.push(CMD::SEND as u8);
     println!("Version Byte: {:02X?}", frame[1]);
 
-    // Command Byte
-    frame.push(0x06); // 命令 byte，假設為 6
+    // Command Byte, Ack=0x01, Nack=0x02, Motor=0x03, SetID=0x04, File=0x05, Sensor High=0x06, Sensor Low=0x07
+    frame.push(Command::MOTOR as u8);
     println!("Command Byte: {:02X?}", frame[2]);
 
     let len = payload.len() as u16;
@@ -67,8 +85,38 @@ fn main() -> anyhow::Result<()> {
     };
     println!("使用串口: {}", port_name);
     // 1️⃣ 準備資料
-    let m1 = Motion {name: "PMt".into(),id: 5,motion: 1,speed: 100,tol: 5,dist: 2000,angle: 100,time: 5000,acc: 300,newid: 0,volt: 12.0,amp: 0.5,temp: 25.0,mode: 0};
-    let m2 = Motion {name: "PMb".into(),id: 4,motion: 1,speed: 100,tol: 2,dist: 1900,angle: 60,time: 4000,acc: 400,newid: 0,volt: 12.0,amp: 0.6,temp: 26.0,mode: 0};
+    let m1 = Motion {
+        name: "PMt".into(),
+        id: 5,
+        motion: 1,
+        speed: 100,
+        tol: 5,
+        dist: 2000,
+        angle: 100,
+        time: 5000,
+        acc: 300,
+        newid: 0,
+        volt: 12.0,
+        amp: 0.5,
+        temp: 25.0,
+        mode: 0,
+    };
+    let m2 = Motion {
+        name: "PMb".into(),
+        id: 4,
+        motion: 1,
+        speed: 100,
+        tol: 2,
+        dist: 1900,
+        angle: 60,
+        time: 4000,
+        acc: 400,
+        newid: 0,
+        volt: 12.0,
+        amp: 0.6,
+        temp: 26.0,
+        mode: 0,
+    };
     let payload = vec![m1, m2]; // 對應外層 3 元素陣列
     println!("Payload: {:?}", payload);
     let payload_cbor: Vec<u8> = serde_cbor::to_vec(&payload)?;
@@ -96,77 +144,98 @@ fn main() -> anyhow::Result<()> {
     let start_time = std::time::Instant::now();
     let mut line = String::new();
     let mut times = 1;
+    let mut idx = 0; // 用於追蹤 START_BYTE 的索引位置
+    let mut buffer = [0u8; MAX_DATA_LEN]; // 用於存儲接收的資料
     loop {
         let mut buf = [0u8; 1];
         if port.read(&mut buf).is_ok() {
-            let ch = buf[0] as char;
-            if ch == '\n' {
-                match line.trim() {
-                    "STATUS" => println!("{} ✓ Arduino 收到封包", line.trim()),
-                    "good" => {
-                        println!("{} ✓ Arduino 處理成功", line.trim());
-                        let elapsed = start_time.elapsed();
-                        println!(
-                            "總共花費時間: {:.2?}, 次數: {} 平均: {:.2?}",
-                            elapsed,
-                            times,
-                            elapsed / times
-                        );
-                        sleep(Duration::from_millis(1));
-                        // port.write_all(b"SEND\n")?;
-                        port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-                        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-                        port.flush()?; // 確保資料已經寫入
-                        times += 1;
-                        if times > 1 {
-                            break;
-                        }
-                    }
-                    "starting..." => {
-                        println!("{} ✓ Arduino 開始處理", line.trim());
-                        // port.write_all(b"SEND\n")?;
-                        port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-                        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-                        port.flush()?; // 確保資料已經寫入
-                        times += 1;
-                    }
-                    "Arduino CBOR Receiver Ready" => {
-                        println!("{} ✓ Arduino 已準備好接收 CBOR 資料", line.trim());
-                        port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-                        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-                        port.flush()?; // 確保資料已經寫入
-                        times += 1;
-                    }
-                    "CBOR Motor Receiver Ready" => {
-                        println!("{} ✓ Arduino 已準備好接收馬達資料", line.trim());
-                        port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-                        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-                        port.flush()?; // 確保資料已經寫入
-                        times += 1;
-                    }
-                    "Ready for next frame" => {
-                        let elapsed = start_time.elapsed();
-                        println!("{} ✓ Arduino 準備好接收下一個幀, 平均耗時: {:.2?}, 次數: {}", line.trim(), elapsed / times, times);
-                        sleep(Duration::from_millis(1));
-                        port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-                        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-                        port.flush()?; // 確保資料已經寫入
-                        times += 1;
-                    }
-                    "[OK ]" => {
-                        println!("{} ✓ Arduino 回應 OK", line.trim());
-                        let elapsed = start_time.elapsed();
-                        println!("總共花費時間: {:.2?}, 次數: {}", elapsed, times);
-                        break;
-                    }
-                    other => {
-                        println!("ℹ️  其他訊息: {other}");
-                    }
-                }
-                line.clear();
-            } else {
-                line.push(ch);
+            let ch = buf[0] as u8;
+            print!("{:02X}", ch);
+            if ch == START_BYTE {
+                // NOTE: 清空 buffer
+                buffer = [0u8; MAX_DATA_LEN];
+                idx = 0;
+                buffer[idx] = ch;
+                continue;
             }
+            if idx == 0 && ch == Command::ACK as u8 {
+                idx += 1;
+                buffer[idx] = ch;
+                continue;
+            }
+
+            if idx <= 3 {
+                idx += 1;
+                buffer[idx] = ch;
+            } 
+
+            // if ch == '\n' {
+            //     match line.trim() {
+            //         "STATUS" => println!("{} ✓ Arduino 收到封包", line.trim()),
+            //         "good" => {
+            //             println!("{} ✓ Arduino 處理成功", line.trim());
+            //             let elapsed = start_time.elapsed();
+            //             println!(
+            //                 "總共花費時間: {:.2?}, 次數: {} 平均: {:.2?}",
+            //                 elapsed,
+            //                 times,
+            //                 elapsed / times
+            //             );
+            //             sleep(Duration::from_millis(1));
+            //             // port.write_all(b"SEND\n")?;
+            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
+            //             port.flush()?; // 確保資料已經寫入
+            //             times += 1;
+            //             if times > 1 {
+            //                 break;
+            //             }
+            //         }
+            //         "starting..." => {
+            //             println!("{} ✓ Arduino 開始處理", line.trim());
+            //             // port.write_all(b"SEND\n")?;
+            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
+            //             port.flush()?; // 確保資料已經寫入
+            //             times += 1;
+            //         }
+            //         "Arduino CBOR Receiver Ready" => {
+            //             println!("{} ✓ Arduino 已準備好接收 CBOR 資料", line.trim());
+            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
+            //             port.flush()?; // 確保資料已經寫入
+            //             times += 1;
+            //         }
+            //         "CBOR Motor Receiver Ready" => {
+            //             println!("{} ✓ Arduino 已準備好接收馬達資料", line.trim());
+            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
+            //             port.flush()?; // 確保資料已經寫入
+            //             times += 1;
+            //         }
+            //         "Ready for next frame" => {
+            //             let elapsed = start_time.elapsed();
+            //             println!("{} ✓ Arduino 準備好接收下一個幀, 平均耗時: {:.2?}, 次數: {}", line.trim(), elapsed / times, times);
+            //             sleep(Duration::from_millis(1));
+            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
+            //             port.flush()?; // 確保資料已經寫入
+            //             times += 1;
+            //         }
+            //         "[OK ]" => {
+            //             println!("{} ✓ Arduino 回應 OK", line.trim());
+            //             let elapsed = start_time.elapsed();
+            //             println!("總共花費時間: {:.2?}, 次數: {}", elapsed, times);
+            //             break;
+            //         }
+            //         other => {
+            //             println!("ℹ️  其他訊息: {other}");
+            //         }
+            //     }
+            //     line.clear();
+            // } else {
+            //     line.push(ch);
+            // }
         }
 
         // port.write_all(b"SEND\n")?;
