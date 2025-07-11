edb1954afcbab4ddc5e2f24e3b8778c0ae2080e9
YueLi <green07111@gmail.com>
2025-07-12 00:39:50 +0800

    [refactor] 新增 CMD 和 Command 列舉，重構資料框架以支持指令處理

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,27 @@
-use std::{ thread::sleep, time::Duration, vec };
+mod model;
+
+use std::{ collections::HashMap, thread::sleep, time::Duration, vec };
 #[allow(unused_imports)]
 use cobs::encode;
-use serde::{ Serialize, Deserialize };
 use crc::{ Crc, CRC_16_USB };
+use serde_cbor::Value;
+use model::{ CMD, Command, Motion };
 
 const BAUD: u32 = 460_800;
 const CRC16: Crc<u16> = Crc::<u16>::new(&CRC_16_USB);
 const START_BYTE: u8 = 0x7e; // 開始 byte
-const MAX_DATA_LEN: usize = 256;
+const MAX_DATA_LEN: usize = 1024;
 
-enum CMD {
-    SEND = 0xaa, // 發送資料
-    READ = 0xa8, // 讀取資料
-}
-
-#[repr(u8)]
-enum Command {
-    ACK = 0x01, // 確認收到
-    NACK = 0x02, // 未確認收到
-    MOTOR = 0x03, // 馬達控制
-    SetID = 0x04, // 設定 ID
-    FILE = 0x05, // 檔案傳輸
-    SensorHIGH = 0x06, // 高位元感測器
-    SensorLOW = 0x07, // 低位元感測器
-}
-
-/// 對應 Arduino encode_cbor() 內兩個 9 元素子陣列
-#[derive(Debug, Serialize, Deserialize)]
-struct Motion {
-    name: String, // 馬達名子 string "PMt" / "PMb"
-    id: u8, // 數值 int8
-    motion: u8, // 馬達動作，數值int8，0: 停止，1:轉動
-    speed: i64, // 設定的速度，數值 int64，有正負
-    tol: u8, // %誤差範圍，數值int8，0~100
-    dist: u32, // 距離，數值 int64
-    angle: u32, // 轉動角度，數值int64，0~359
-    time: u32, // 轉動時間，數值int64, ms
-    acc: u32, // 加速度，數值 int64
-    newid: u8, // 改變後新id，數值 int8
-    volt: f32, // 電壓， float
-    amp: f32, // 電流， float
-    temp: f32, // 溫度， float
-    mode: u8, // 馬達運行模式，數值int8，0:default，1:位置，2:速度
-}
-
-fn build_frame(payload: &[u8]) -> Vec<u8> {
+fn build_frame(cmd: CMD, command: Command, payload: &[u8]) -> Vec<u8> {
     let mut frame = Vec::with_capacity(payload.len() + 5);
 
     frame.push(START_BYTE); // 開始 byte
     // Cmd Byte, SEND: 0xAA, READ: 0xA8
-    frame.push(CMD::SEND as u8);
-    println!("Version Byte: {:02X?}", frame[1]);
+    frame.push(cmd as u8);
+    println!("Cmd Byte: {:02X?}", frame[1]);
 
     // Command Byte, Ack=0x01, Nack=0x02, Motor=0x03, SetID=0x04, File=0x05, Sensor High=0x06, Sensor Low=0x07
-    frame.push(Command::MOTOR as u8);
+    frame.push(command as u8);
     println!("Command Byte: {:02X?}", frame[2]);
 
     let len = payload.len() as u16;
@@ -62,7 +30,7 @@ fn build_frame(payload: &[u8]) -> Vec<u8> {
 
     frame.extend(payload);
 
-    let crc = CRC16.checksum(&frame[3..]); // 跳過 START_BYTE, Version Byte, Command Byte
+    let crc = CRC16.checksum(&frame[3..]); // 跳過 START_BYTE, Cmd Byte, Command Byte
     frame.extend(crc.to_le_bytes()); // lo, hi
     println!("CRC: {} {:02X?}", crc, crc.to_le_bytes());
 
@@ -71,11 +39,11 @@ fn build_frame(payload: &[u8]) -> Vec<u8> {
 
 fn main() -> anyhow::Result<()> {
     let payload = 0; // 空的 payload
-    println!("Empty PayLoad: {}", payload);
+    println!("Sensor PayLoad: {}", payload);
     let payload_cbor = serde_cbor::to_vec(&payload)?;
-    println!("Empty PayLoad CBOR: {:02X?}", payload_cbor);
-    let frame = build_frame(&payload_cbor);
-    println!("Empty Frame: {:02X?}", frame);
+    println!("Sensor PayLoad CBOR: {:02X?}", payload_cbor);
+    let frame = build_frame(CMD::READ, Command::SensorHIGH, &payload_cbor);
+    println!("Sensor Frame: {:02X?}, len: {}", frame, frame.len());
 
     let args: Vec<String> = std::env::args().collect();
     let port_name = if args.len() > 1 {
@@ -117,132 +85,242 @@ fn main() -> anyhow::Result<()> {
         temp: 26.0,
         mode: 0,
     };
-    let payload = vec![m1, m2]; // 對應外層 3 元素陣列
+    let payload: Vec<Motion> = vec![m1, m2]; // 對應外層 3 元素陣列
     println!("Payload: {:?}", payload);
     let payload_cbor: Vec<u8> = serde_cbor::to_vec(&payload)?;
     println!("CBOR 資料: {:02X?}", payload_cbor);
     println!("CBOR 長度: {}", payload_cbor.len());
-    let frame = build_frame(&payload_cbor);
+    let frame = build_frame(CMD::SEND, Command::MOTOR, &payload_cbor);
     let dst_frame = frame.clone();
     // let mut dst_frame = vec![0; frame.len() + 1]; // COBS 編碼後長度會增加
     // let _encoded_size = encode(&frame, &mut dst_frame);
     // // println!("Encoded Frame size: {}", encoded_size);
-    // println!("Encoded Frame: {:02X?}", dst_frame);
+    println!("Encoded Frame: {:02X?}", dst_frame);
+    let timeout = Duration::from_secs(1);
     // 2️⃣ 打開序列埠
-    let mut port = serialport
-        ::new(port_name, BAUD)
-        .timeout(Duration::from_millis(500))
-        .open()
-        .expect("Failed to open serial port");
-
-    // port.write_all(b"SEND\n")?;
+    for port in serialport::available_ports()? {
+        println!("Found port: {}", port.port_name);
+    }
+    let port_result = serialport::new(port_name, BAUD).timeout(timeout).open();
+    let mut port = match port_result {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("無法打開序列埠 {}: {}", port_name, e);
+            return Err(anyhow::anyhow!("無法打開序列埠"));
+        }
+    };
+    println!("成功打開序列埠: {}", port_name);
+    println!("等待 1 秒鐘...");
+    sleep(Duration::from_secs(1));
+    // 3️⃣ 傳送資料
+    println!("傳送資料: {:02X?}", dst_frame);
     port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-    // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
     port.flush()?; // 確保資料已經寫入
 
     // 4️⃣ 等待回覆
+    println!("等待回覆...");
     let start_time = std::time::Instant::now();
-    let mut line = String::new();
+
     let mut times = 1;
     let mut idx = 0; // 用於追蹤 START_BYTE 的索引位置
-    let mut buffer = [0u8; MAX_DATA_LEN]; // 用於存儲接收的資料
+    let mut buffer = Vec::<u8>::with_capacity(MAX_DATA_LEN); // 用於存儲接收的資料
+
+    let mut cmd = CMD::SEND; // 初始 CMD
+    let mut command = Command::ACK; // 初始 Command
+    let mut len_bytes = [0u8; 2]; // 用於存儲長度的 byte
+    let mut payload_size = 0 as usize;
+    let mut payload_data = Vec::<u8>::new(); // 用於存儲 payload 部分
+    let mut crc_bytes = [0u8; 2]; // 用於存儲 CRC 的 byte
+
+    let max_retries = 5; // 最大重試次數
+    let mut retries = 0; // 當前重試次數
     loop {
         let mut buf = [0u8; 1];
-        if port.read(&mut buf).is_ok() {
-            let ch = buf[0] as u8;
-            print!("{:02X}", ch);
-            if ch == START_BYTE {
-                // NOTE: 清空 buffer
-                buffer = [0u8; MAX_DATA_LEN];
-                idx = 0;
-                buffer[idx] = ch;
-                continue;
+        let read_result = port.read(&mut buf);
+        if read_result.is_err() {
+            println!("讀取串口資料失敗，可能是串口已關閉或發生錯誤");
+            // 嘗試關閉並重新打開串口
+            drop(port);
+            println!("關閉序列埠: {}", port_name);
+            sleep(Duration::from_secs(1)); // 等待 1 秒鐘
+            // 嘗試重新打開串口
+            loop {
+                match serialport::new(port_name, BAUD).timeout(timeout).open() {
+                    Ok(p) => {
+                        port = p;
+                        println!("成功重新打開序列埠: {}", port_name);
+                        break;
+                    }
+                    Err(e) => {
+                        retries += 1;
+                        if retries >= max_retries {
+                            eprintln!("無法重新打開序列埠 {}: {}", port_name, e);
+                            return Err(anyhow::anyhow!("無法重新打開序列埠"));
+                        }
+                        eprintln!(
+                            "無法重新打開序列埠 {}: {}, 正在重試第 {} 次",
+                            port_name,
+                            e,
+                            retries
+                        );
+                        sleep(Duration::from_secs(1));
+                        continue;
+                    }
+                }
             }
-            if idx == 0 && ch == Command::ACK as u8 {
-                idx += 1;
-                buffer[idx] = ch;
-                continue;
-            }
-
-            if idx <= 3 {
-                idx += 1;
-                buffer[idx] = ch;
-            } 
-
-            // if ch == '\n' {
-            //     match line.trim() {
-            //         "STATUS" => println!("{} ✓ Arduino 收到封包", line.trim()),
-            //         "good" => {
-            //             println!("{} ✓ Arduino 處理成功", line.trim());
-            //             let elapsed = start_time.elapsed();
-            //             println!(
-            //                 "總共花費時間: {:.2?}, 次數: {} 平均: {:.2?}",
-            //                 elapsed,
-            //                 times,
-            //                 elapsed / times
-            //             );
-            //             sleep(Duration::from_millis(1));
-            //             // port.write_all(b"SEND\n")?;
-            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-            //             port.flush()?; // 確保資料已經寫入
-            //             times += 1;
-            //             if times > 1 {
-            //                 break;
-            //             }
-            //         }
-            //         "starting..." => {
-            //             println!("{} ✓ Arduino 開始處理", line.trim());
-            //             // port.write_all(b"SEND\n")?;
-            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-            //             port.flush()?; // 確保資料已經寫入
-            //             times += 1;
-            //         }
-            //         "Arduino CBOR Receiver Ready" => {
-            //             println!("{} ✓ Arduino 已準備好接收 CBOR 資料", line.trim());
-            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-            //             port.flush()?; // 確保資料已經寫入
-            //             times += 1;
-            //         }
-            //         "CBOR Motor Receiver Ready" => {
-            //             println!("{} ✓ Arduino 已準備好接收馬達資料", line.trim());
-            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-            //             port.flush()?; // 確保資料已經寫入
-            //             times += 1;
-            //         }
-            //         "Ready for next frame" => {
-            //             let elapsed = start_time.elapsed();
-            //             println!("{} ✓ Arduino 準備好接收下一個幀, 平均耗時: {:.2?}, 次數: {}", line.trim(), elapsed / times, times);
-            //             sleep(Duration::from_millis(1));
-            //             port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-            //             // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-            //             port.flush()?; // 確保資料已經寫入
-            //             times += 1;
-            //         }
-            //         "[OK ]" => {
-            //             println!("{} ✓ Arduino 回應 OK", line.trim());
-            //             let elapsed = start_time.elapsed();
-            //             println!("總共花費時間: {:.2?}, 次數: {}", elapsed, times);
-            //             break;
-            //         }
-            //         other => {
-            //             println!("ℹ️  其他訊息: {other}");
-            //         }
-            //     }
-            //     line.clear();
-            // } else {
-            //     line.push(ch);
-            // }
+            println!("重新打開序列埠: {}", port_name);
+            sleep(Duration::from_secs(1)); // 等待 1 秒鐘
+            continue; // 重新開始循環
         }
-
-        // port.write_all(b"SEND\n")?;
-        // port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
-        // port.write_all(&[0x00])?; // COBS 編碼後的結尾 byte
-        // port.flush()?; // 確保資料已經寫入
-        // times += 1;
+        if read_result.is_ok() {
+            let ch = buf[0] as u8;
+            // println!("接收到 byte: {:02X?}", ch);
+            // sleep(Duration::from_millis(100));
+            match ch {
+                START_BYTE => {
+                    // println!("idx: {}, 接收到 byte: {:02X?}", idx, ch);
+                    idx = 0; // 重置索引
+                    buffer.clear(); // 清空 buffer
+                    len_bytes = [0u8; 2]; // 重置長度 byte
+                    crc_bytes = [0u8; 2]; // 重置 CRC byte
+                    continue; // 跳過後續處理
+                }
+                val if idx == 0 && CMD::try_from(val).is_ok() => {
+                    idx += 1; // 確認收到 CMD
+                    buffer.push(ch);
+                    cmd = CMD::try_from(val).unwrap();
+                    // println!("idx: {}, 收到 CMD: {:02X?}", idx, cmd);
+                    continue; // 跳過後續處理
+                }
+                val if idx == 1 && Command::try_from(val).is_ok() => {
+                    idx += 1; // 確認收到 Command
+                    buffer.push(ch);
+                    command = Command::try_from(val).unwrap();
+                    // println!("idx: {}, 收到 CMD: {:02X?}", idx, command);
+                    continue; // 跳過後續處理
+                }
+                _val if idx == 2 => {
+                    idx += 1; // 確認收到長度 byte
+                    buffer.push(ch);
+                    len_bytes[0] = ch; // 第一個長度 byte
+                    continue; // 跳過後續處理
+                }
+                _val if idx == 3 => {
+                    idx += 1; // 確認收到第二個長度 byte
+                    buffer.push(ch);
+                    len_bytes[1] = ch; // 第二個長度 byte
+                    payload_size = u16::from_le_bytes(
+                        len_bytes.as_slice().try_into().unwrap()
+                    ) as usize;
+                    if payload_size > MAX_DATA_LEN {
+                        println!("Payload 長度超過最大限制: {}", MAX_DATA_LEN);
+                        idx = 0; // 重置索引
+                        buffer.clear(); // 清空 buffer
+                        len_bytes = [0u8; 2]; // 重置長度 byte
+                        crc_bytes = [0u8; 2]; // 重置 CRC byte
+                        continue; // 跳過後續處理
+                    }
+                    // println!(
+                    //     "idx: {}, 收到長度: {:02X?}, Payload 長度: {}",
+                    //     idx,
+                    //     len_bytes,
+                    //     payload_size
+                    // );
+                    continue; // 跳過後續處理
+                }
+                _val if idx >= 4 && idx < 4 + payload_size => {
+                    idx += 1; // 繼續接收 payload 部分
+                    buffer.push(ch);
+                    payload_data.push(ch);
+                    // println!(
+                    //     "idx: {}, 收到 payload: {:02X?}, ",
+                    //     idx,
+                    //     buffer[4..4 + payload_size].to_vec()
+                    // );
+                    continue; // 跳過後續處理
+                }
+                _val if idx == 4 + payload_size => {
+                    idx += 1; // 確認收到第一個 CRC byte
+                    buffer.push(ch);
+                    crc_bytes[0] = ch; // 第一個 CRC byte
+                    continue; // 跳過後續處理
+                }
+                _val if idx == 4 + payload_size + 1 => {
+                    idx += 1; // 確認收到第二個 CRC byte
+                    buffer.push(ch);
+                    crc_bytes[1] = ch; // 第二個 CRC byte
+                    // println!("idx: {}, 收到 CRC bytes: {:02X?}", idx, crc_bytes);
+                    let crc = u16::from_le_bytes(crc_bytes.as_slice().try_into().unwrap());
+                    // println!("接收到的 CRC: {:04X}", crc);
+                    let calc_crc = CRC16.checksum(&buffer[2..4 + payload_size]); // 跳過 START_BYTE, Cmd Byte, Command Byte
+                    // println!("計算的 CRC: {:04X}", calc_crc);
+                    if crc == calc_crc {
+                        // println!("CRC 驗證成功");
+                        // 將 payload 部分轉換為 CBOR 資料
+                        // let mut result = None;
+                        if
+                            payload_size == 1 &&
+                            (command == Command::SensorHIGH || command == Command::SensorLOW)
+                        {
+                            // payload 不重要
+                            // result = Some(format!("Sensor Command: {}", command));
+                            let elapsed = start_time.elapsed();
+                            println!(
+                                "平均耗時: {:.2?}, 次數: {}, 接收到的資料: {}",
+                                elapsed / times,
+                                times,
+                                format!("Sensor cmd: {}, Command: {}", cmd, command)
+                            );
+                            port.write_all(&dst_frame)?; // 傳送 COBS 編碼後的資料
+                            port.flush()?; // 確保資料已經寫入
+                        } else {
+                            let decoded_payload: HashMap<String, Value> = serde_cbor::from_slice(
+                                &buffer[4..4 + payload_size]
+                            )?;
+                            // result = Some(
+                            //     format!("Payload: {:?}, Command: {}", decoded_payload, command)
+                            // );
+                            let elapsed = start_time.elapsed();
+                            println!(
+                                "平均耗時: {:.2?}, 次數: {}, 接收到的資料: {}",
+                                elapsed / times,
+                                times,
+                                format!(
+                                    "Payload: {:?}, cmd: {}, Command: {}",
+                                    decoded_payload,
+                                    cmd,
+                                    command
+                                )
+                            );
+                        }
+                        // let elapsed = start_time.elapsed();
+                        // println!(
+                        //     "平均耗時: {:.2?}, 次數: {}, 接收到的資料: {}",
+                        //     elapsed / times,
+                        //     times,
+                        //     result.clone().unwrap()
+                        // );
+                        // if !result.unwrap().contains("Sensor Command") {
+                        //     break;
+                        // }
+                        times += 1;
+                        continue; // 跳過後續處理
+                    } else {
+                        // println!("CRC 驗證失敗");
+                        idx = 0; // 重置索引
+                        buffer.clear(); // 清空 buffer
+                        len_bytes = [0u8; 2]; // 重置長度 byte
+                        crc_bytes = [0u8; 2]; // 重置 CRC byte
+                        continue; // 跳過後續處理
+                    }
+                }
+                _ => {
+                    idx += 1; // 增加索引
+                    buffer.push(ch); // 將接收到的 byte 添加到 buffer
+                    continue; // 跳過後續處理
+                }
+            }
+        }
     }
 
     Ok(())
---
+++ src/model.rs
@@ -0,0 +1,81 @@
+use std::fmt::Display;
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum CMD {
+    SEND = 0xaa, // 發送資料
+    READ = 0xa8, // 讀取資料
+}
+
+impl TryFrom<u8> for CMD {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0xaa => Ok(CMD::SEND),
+            0xa8 => Ok(CMD::READ),
+            _ => Err("Invalid CMD Byte"),
+        }
+    }
+}
+
+impl Display for CMD {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum Command {
+    ACK = 0x01, // 確認收到
+    NACK = 0x02, // 未確認收到
+    MOTOR = 0x03, // 馬達控制
+    SetID = 0x04, // 設定 ID
+    FILE = 0x05, // 檔案傳輸
+    SensorHIGH = 0x06, // 高位元感測器
+    SensorLOW = 0x07, // 低位元感測器
+}
+
+impl TryFrom<u8> for Command {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x01 => Ok(Command::ACK),
+            0x02 => Ok(Command::NACK),
+            0x03 => Ok(Command::MOTOR),
+            0x04 => Ok(Command::SetID),
+            0x05 => Ok(Command::FILE),
+            0x06 => Ok(Command::SensorHIGH),
+            0x07 => Ok(Command::SensorLOW),
+            _ => Err("Invalid Command Byte"),
+        }
+    }
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// 對應 Arduino encode_cbor() 內兩個 9 元素子陣列
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Motion {
+    pub name: String, // 馬達名子 string "PMt" / "PMb"
+    pub id: u8, // 數值 int8
+    pub motion: u8, // 馬達動作，數值int8，0: 停止，1:轉動
+    pub speed: i64, // 設定的速度，數值 int64，有正負
+    pub tol: u8, // %誤差範圍，數值int8，0~100
+    pub dist: u32, // 距離，數值 int64
+    pub angle: u32, // 轉動角度，數值int64，0~359
+    pub time: u32, // 轉動時間，數值int64, ms
+    pub acc: u32, // 加速度，數值 int64
+    pub newid: u8, // 改變後新id，數值 int8
+    pub volt: f32, // 電壓， float
+    pub amp: f32, // 電流， float
+    pub temp: f32, // 溫度， float
+    pub mode: u8, // 馬達運行模式，數值int8，0:default，1:位置，2:速度
+}
